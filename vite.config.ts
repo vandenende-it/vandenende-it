ad27ed2642da2f6233d58d6fd469989a9c7a054c
Maarten van den Ende <maarten@vandenende.it>
2025-11-22 13:51:56 +0100

    css

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      plugins: [react(), tailwindcss()],
+      plugins: [react()],
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
